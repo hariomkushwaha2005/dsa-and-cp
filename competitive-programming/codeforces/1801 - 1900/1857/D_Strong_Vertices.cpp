f6aa587412f38cb7e441ece24f016fd86b3600ab
Hariom Kushwaha <ihari.kush18@gmail.com>
2026-08-03 02:21:55 +0530

    initial commit

---
+++ competitive-programming/codeforces/1801 - 1900/1857/D_Strong_Vertices.cpp
@@ -0,0 +1,62 @@
+#pragma GCC optimize("O3,unroll-loops")
+// #pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+
+inline void fast_io()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+}
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    vll v(n);
+    for (int i = 0; i < n; ++i)
+        cin >> v[i];
+    ll maxx = LLONG_MIN;
+    int cnt = 0;
+    for (int i = 0; i < n; ++i)
+    {
+        int x;
+        cin >> x;
+        v[i] -= x;
+        if (maxx < v[i])
+        {
+            maxx = v[i];
+            cnt = 1;
+        }
+        else if(maxx == v[i])cnt++;
+    }
+
+    cout << cnt << endl;
+    for(int i = 0; i < n; ++i){
+        if(v[i] == maxx) cout << i + 1 << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    fast_io();
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
