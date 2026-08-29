7ea4dbdfd55d8218d8210e6bc29e1ad700980f77
Hariom Kushwaha <ihari.kush18@gmail.com>
2026-08-29 18:53:32 +0530

    initial commit

--- competitive-programming/codeforces/1601 - 1700/1634/B_Fortune_Telling.cpp
+++ competitive-programming/codeforces/1601 - 1700/1634/B_Fortune_Telling.cpp
@@ -1,4 +1,6 @@
 #include <bits/stdc++.h>
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
 
 using namespace std;
 using ll = long long;
@@ -13,6 +15,19 @@ inline void fast_io()
 
 void solve()
 {
+    ll n, x, y;
+    cin >> n >> x >> y;
+
+    ll cnt = 0;
+    for (int i = 0; i < n; ++i)
+    {
+        ll z;
+        cin >> z;
+
+        cnt += (z & 1);
+    }
+
+    cout << (((x ^ y ^ cnt) & 1) ? "Bob\n" : "Alice\n");
 }
 
 int main()
