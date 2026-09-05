2be512694f4af8aa510776e67da5d6bef577f710
Hariom Kushwaha <ihari.kush18@gmail.com>
2026-09-05 20:13:16 +0530

    initial commit

---
+++ competitive-programming/codeforces/1301 - 1400/1362/C_Johnny_and_Another_Rating_Drop.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+
+using namespace std;
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+
+inline void fast_io()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+}
+
+void solve()
+{
+    ll n;
+    cin >> n;
+
+    ll a = 1, ans = 0;
+    while (a <= n)
+    {
+        if (a & n)
+            ans += (a << 1) - 1;
+        a <<= 1;
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    fast_io();
+
+    int t = 1;
+    cin >> t;
+
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
